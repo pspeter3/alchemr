9e936efd6ed6dc05bc4f0f9151b1ce06af93a604
Phips Peter <pspeter333@gmail.com>
2012-02-16 14:42:42 -0600

    Initial commit

---
+++ spec/spec_helper.rb
@@ -0,0 +1,19 @@
+PADRINO_ENV = 'test' unless defined?(PADRINO_ENV)
+require File.expand_path(File.dirname(__FILE__) + "/../config/boot")
+
+RSpec.configure do |conf|
+  conf.include Rack::Test::Methods
+  conf.include Mongoid::Matchers
+  conf.after :suite do
+    Mongoid.master.collections.select do |collection|
+      collection.name !~ /system/
+    end.each(&:drop)
+  end
+end
+
+def app
+  ##
+  # You can handle all padrino applications using instead:
+  #   Padrino.application
+  Krater.tap { |app|  }
+end
