a6eb4cbe904506ed4507e29a2486e86dc76ac90d
Phips Peter <pspeter333@gmail.com>
2012-02-16 15:38:20 -0600

    Adding better alert support

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,4 +1,7 @@
 // Put your application scripts here
 $(function() {
+  // Add close button to flashes
+  $(".alert").prepend('<a class="close" data-dismiss="alert" href="#">&times;</a>');
+  // Adds alert support
   $(".alert-message").alert();
 });
