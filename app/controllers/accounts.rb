3dfd617dc0dd2816e84360438b0e608d8a418279
Phips Peter <pspeter333@gmail.com>
2012-02-16 15:05:19 -0600

    Create accounts controller with REST interface

---
+++ app/controllers/accounts.rb
@@ -0,0 +1,23 @@
+Krater.controllers :accounts do
+  get :index do
+  end
+
+  get :new do
+  end
+
+  post :create, :map => '/accounts' do
+  end
+
+  get :show, :map => '/accounts/:id' do
+  end
+
+  get :edit, :map => '/accounts/:id/edit' do
+  end
+
+  put :update, :map => '/accounts/:id' do
+  end
+
+  delete :destroy, :map => '/accounts/:id' do
+  end
+
+end
