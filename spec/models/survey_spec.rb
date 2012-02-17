1ac062679a915ddfdb1a53cf2719572cadc5eea2
Phips Peter <pspeter333@gmail.com>
2012-02-16 21:24:51 -0600

    Updated the survey spec

--- spec/models/survey_spec.rb
+++ spec/models/survey_spec.rb
@@ -1,8 +1,17 @@
 require 'spec_helper'
 
-describe "Survey Model" do
+describe Survey do
   let(:survey) { Survey.new }
   it 'can be created' do
     survey.should_not be_nil
   end
+  
+  it { should have_fields(:title, :desc).of_type(String) }
+  it { should have_field(:sites).of_type(Array) }
+  
+  it { should validate_presence_of(:title) }
+  
+  it { should embed_many(:questions) }
+  it { should belong_to(:account) }
+  
 end
