d3292786f8132d2315c4a91d46157ff10f64a392
Phips Peter <pspeter333@gmail.com>
2012-02-16 21:16:06 -0600

    Updated the question spec

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,8 +1,18 @@
 require 'spec_helper'
 
-describe "Question Model" do
+describe Question do
   let(:question) { Question.new }
   it 'can be created' do
     question.should_not be_nil
   end
+  
+  it { should have_fields(:prompt, :help).of_type(String) }
+  it { should have_field(:type).of_type(Symbol) }
+  it { should have_field(:config).of_type(Hash) }
+  it { should have_field(:required).of_type(Boolean) }
+  
+  it { should validate_presence_of(:prompt) }
+  it { should validate_presence_of(:type) }
+  
+  it { should belong_to(:survey) }
 end
