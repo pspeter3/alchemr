c49563bd17f4a26f888c171ceff7a0ef7fcd0ad4
Phips Peter <pspeter333@gmail.com>
2012-02-19 13:35:53 -0600

    Updated the website spec

--- spec/models/website_spec.rb
+++ spec/models/website_spec.rb
@@ -1,8 +1,13 @@
 require 'spec_helper'
 
-describe "Website Model" do
+describe Website do
   let(:website) { Website.new }
   it 'can be created' do
     website.should_not be_nil
   end
+  it { should have_fields(:name, :url, :img, :desc).of_type(String) }
+  it { should validate_presence_of(:name) }
+  it { should validate_presence_of(:url) }
+  it 'should have a url Regex constant' { Website.url_regex.should eql /(?i)\b((?:[a-z][\w-]+:(?:\/{1,3}|[a-z0-9%])|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}\/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'".,<>?«»“”‘’]))/ }
+  it { should validate_format_of(:url).with(Website.url_regex) }
 end
