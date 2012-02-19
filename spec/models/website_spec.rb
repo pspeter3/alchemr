b32e103bfb57827cb5385e291276530cbc9d1644
Phips Peter <pspeter333@gmail.com>
2012-02-19 13:37:36 -0600

    Made the website spec better

--- spec/models/website_spec.rb
+++ spec/models/website_spec.rb
@@ -8,6 +8,8 @@ describe Website do
   it { should have_fields(:name, :url, :img, :desc).of_type(String) }
   it { should validate_presence_of(:name) }
   it { should validate_presence_of(:url) }
-  it 'should have a url Regex constant' { Website.url_regex.should eql /(?i)\b((?:[a-z][\w-]+:(?:\/{1,3}|[a-z0-9%])|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}\/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'".,<>?«»“”‘’]))/ }
+  it 'should have a url Regex constant' do
+    Website.url_regex.should eql /(?i)\b((?:[a-z][\w-]+:(?:\/{1,3}|[a-z0-9%])|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}\/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'".,<>?]))/
+  end
   it { should validate_format_of(:url).with(Website.url_regex) }
 end
