7eb1858165d47b634f3567d1f97b7c7275595a22
Phips Peter <pspeter333@gmail.com>
2012-02-26 13:06:58 -0600

    Added a questions validation to survey

--- models/survey.rb
+++ models/survey.rb
@@ -9,8 +9,14 @@ class Survey
   
   validates_presence_of :title
   validates_numericality_of :cap
+  validate :has_questions
   
   embeds_many :questions
   accepts_nested_attributes_for :questions, :reject_if => proc {|attr| attr['prompt'].blank? && attr['options'].empty? }
   belongs_to :account
+  
+  private
+  def has_questions
+    errors.add(:questions, "must have at least one question") if self.questions.empty?
+  end
 end
