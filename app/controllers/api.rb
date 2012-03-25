65f85f2df8d0177d0ae1d773e6ccb0d7a63d5c19
Phips Peter <pspeter333@gmail.com>
2012-03-25 16:28:27 -0500

    Scaffolded the requests controller

--- app/controllers/api.rb
+++ app/controllers/api.rb
@@ -4,8 +4,8 @@ Alchemr.controllers :api do
   get :call, :map => '/api/:key' do
     # Get the website
     @website = Website.where(:key => params[:key]).only(:key, :msg, :restricted).first
-    # Check if it is restricted
     @survey = Survey.where(:users => {"$ne" => current_user[:id]}, :active => true).only(:title, :questions).shuffle[0]
+    @message = "load"
     render 'api/call'
   end
   
---
+++ app/helpers/requests_helper.rb
@@ -0,0 +1,7 @@
+# Helper methods defined here can be accessed in any controller or view in the application
+
+Alchemr.helpers do
+  # def simple_helper_method
+  #  ...
+  # end
+end
